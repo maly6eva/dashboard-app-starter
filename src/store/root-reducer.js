cd82dec37df3d53217a65a4ef4004f9e74beca23
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2025-02-23 10:41:58 +0300

    reducer_filter

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,10 @@
 import {combineReducers} from 'redux'
 
 import {positionReducer} from './positions/position-reducer'
+import {filterReducer} from './filters/filter-reducer'
+
 
 export const rootReducer = combineReducers({
     positions: positionReducer,
+    filters: filterReducer,
 })
